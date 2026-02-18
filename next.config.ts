2af28cdca86d81a537c90c218592f19c4953747f
Brighton He <brighton@MacBookPro.home.arpa>
2026-02-17 18:02:50 -0600

    Fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com', // Allows Google Profile Pictures
+      },
+      {
+        protocol: 'https',
+        hostname: 'openweathermap.org', // Useful if we add weather icons later
+      }
+    ],
+  },
 };
 
 export default nextConfig;
